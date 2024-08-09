98e43344eef91df3f98c947935531afb77792d3d
Gundumanju <gundupagimanjunath@gmail.com>
2024-08-09 12:55:59 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,10 +4,7 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # angelicagardner
-
+   # Author:G Manjunath (User:gpm@bimsadoni.ac.in) 
    # Input:
    # p, principal amount
    # t, time period in years
